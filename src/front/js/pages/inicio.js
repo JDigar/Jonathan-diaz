9b7e01be7ec8455e6dacb2aebbdd79634e515550
JDigar <jdigar96@gmail.com>
2023-09-11 21:13:27 +0000

    pag incio

--- src/front/js/pages/inicio.js
+++ src/front/js/pages/inicio.js
@@ -11,129 +11,16 @@ export const Inicio = () => {
         </div>
       </div>
 
-      <div className="p-4">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam et
-          eros in sapien malesuada rhoncus at nec libero. Maecenas dictum
-          posuere nisl in maximus. Morbi turpis neque, ultricies a tincidunt
-          vitae, ornare vitae sem. Curabitur pulvinar molestie ornare. Curabitur
-          molestie turpis luctus lorem pulvinar, ac aliquet elit luctus. Aliquam
-          maximus pretium orci eget scelerisque. Interdum et malesuada fames ac
-          ante ipsum primis in faucibus. Quisque pretium diam quis lorem dictum,
-          ac pretium quam fringilla. Etiam eu tellus odio. Morbi nec justo eget
-          mauris hendrerit malesuada sed posuere quam. Vestibulum ante ipsum
-          primis in faucibus orci luctus et ultrices posuere cubilia curae;
-          Praesent non ante finibus, pharetra eros a, tristique elit. Aliquam
-          efficitur nibh tellus, nec sagittis orci varius non. Donec et
-          tincidunt arcu, id varius felis. Vestibulum id leo vitae elit
-          condimentum suscipit. Donec dui velit, tristique ut luctus nec,
-          bibendum id libero. In ac nisl sed sapien sodales ornare. Aenean
-          ornare massa lacus, ut imperdiet turpis rhoncus at. Curabitur arcu
-          arcu, mattis hendrerit molestie ac, posuere eget massa. Mauris
-          pretium, quam vitae imperdiet placerat, leo diam mattis quam, ac
-          molestie turpis purus sagittis libero. In maximus commodo maximus.
-          Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras
-          sagittis laoreet dapibus. Phasellus fermentum tempor tincidunt. Aenean
-          lobortis ligula lectus, sed ultrices elit suscipit porta. Vestibulum
-          non ultricies ante. Phasellus sodales volutpat tincidunt. Aenean
-          tempor efficitur neque. Nulla facilisi. Pellentesque viverra augue
-          sem, at tempor ante fringilla vitae. Etiam semper massa sit amet diam
-          convallis, et placerat ligula pretium. Nulla sed consequat ante. In
-          congue erat sit amet euismod porttitor. Maecenas ut risus nec sapien
-          consequat pellentesque ac ut libero. Nulla facilisi. Curabitur iaculis
-          diam a augue molestie iaculis sit amet sed dui. Nulla metus diam,
-          lacinia a odio non, iaculis ornare quam. Ut nec libero tempor neque
-          pretium finibus. Quisque cursus, magna vel pharetra dignissim, nunc
-          sapien imperdiet arcu, vitae porta magna nulla quis dolor. Aenean
-          faucibus feugiat ornare. Nulla facilisi. Duis eget velit condimentum,
-          ullamcorper felis ut, varius urna. Sed lacinia tortor arcu, eget
-          ultrices est laoreet vel. Aliquam eget dui non mauris bibendum rutrum.
-          Suspendisse non sodales lorem. Sed consequat commodo nulla sed
-          maximus. Ut convallis tempor lorem, nec lacinia nulla rhoncus vel.
-          Orci varius natoque penatibus et magnis dis parturient montes,
-          nascetur ridiculus mus. Sed ac pharetra ligula. Aliquam consectetur
-          dolor nunc, pharetra facilisis augue malesuada quis. Vivamus cursus
-          lorem nec imperdiet suscipit. Suspendisse volutpat ultrices metus non
-          tristique. Sed pharetra mi non felis tempus, eget aliquam sem
-          scelerisque. Pellentesque suscipit, sapien sit amet semper semper,
-          massa est auctor nisl, a euismod lectus purus et urna. Vivamus et
-          rutrum risus. Donec interdum, purus quis ullamcorper maximus, eros
-          lacus placerat ex, quis dictum dolor magna eu urna. Integer aliquam,
-          quam in facilisis tempus, leo massa cursus orci, ac lobortis mi elit
-          id felis. Nullam molestie malesuada efficitur. Vivamus mi lectus,
-          posuere in feugiat eu, consequat ut nibh. Nam consectetur sapien eu
-          convallis hendrerit. Etiam placerat, leo vitae fringilla mollis, diam
-          mauris vulputate massa, sed venenatis ante libero sit amet lorem.
-          Aliquam ipsum libero, iaculis eu dolor eget, rutrum pulvinar massa.
-          Vestibulum sed lorem orci. Aenean eget tempus ex. Sed tincidunt purus
-          quis ullamcorper ornare. Etiam ac vestibulum odio. Phasellus eu felis
-          quis nisi mollis hendrerit. Nulla consequat odio ac ornare
-          scelerisque. Vestibulum et lorem velit. Ut sit amet erat tincidunt,
-          viverra metus nec, fringilla ipsum. Aliquam sed lorem tristique,
-          eleifend risus fermentum, molestie quam.
-        </p>
-      </div>
-      <div className="p-4">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam et
-          eros in sapien malesuada rhoncus at nec libero. Maecenas dictum
-          posuere nisl in maximus. Morbi turpis neque, ultricies a tincidunt
-          vitae, ornare vitae sem. Curabitur pulvinar molestie ornare. Curabitur
-          molestie turpis luctus lorem pulvinar, ac aliquet elit luctus. Aliquam
-          maximus pretium orci eget scelerisque. Interdum et malesuada fames ac
-          ante ipsum primis in faucibus. Quisque pretium diam quis lorem dictum,
-          ac pretium quam fringilla. Etiam eu tellus odio. Morbi nec justo eget
-          mauris hendrerit malesuada sed posuere quam. Vestibulum ante ipsum
-          primis in faucibus orci luctus et ultrices posuere cubilia curae;
-          Praesent non ante finibus, pharetra eros a, tristique elit. Aliquam
-          efficitur nibh tellus, nec sagittis orci varius non. Donec et
-          tincidunt arcu, id varius felis. Vestibulum id leo vitae elit
-          condimentum suscipit. Donec dui velit, tristique ut luctus nec,
-          bibendum id libero. In ac nisl sed sapien sodales ornare. Aenean
-          ornare massa lacus, ut imperdiet turpis rhoncus at. Curabitur arcu
-          arcu, mattis hendrerit molestie ac, posuere eget massa. Mauris
-          pretium, quam vitae imperdiet placerat, leo diam mattis quam, ac
-          molestie turpis purus sagittis libero. In maximus commodo maximus.
-          Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras
-          sagittis laoreet dapibus. Phasellus fermentum tempor tincidunt. Aenean
-          lobortis ligula lectus, sed ultrices elit suscipit porta. Vestibulum
-          non ultricies ante. Phasellus sodales volutpat tincidunt. Aenean
-          tempor efficitur neque. Nulla facilisi. Pellentesque viverra augue
-          sem, at tempor ante fringilla vitae. Etiam semper massa sit amet diam
-          convallis, et placerat ligula pretium. Nulla sed consequat ante. In
-          congue erat sit amet euismod porttitor. Maecenas ut risus nec sapien
-          consequat pellentesque ac ut libero. Nulla facilisi. Curabitur iaculis
-          diam a augue molestie iaculis sit amet sed dui. Nulla metus diam,
-          lacinia a odio non, iaculis ornare quam. Ut nec libero tempor neque
-          pretium finibus. Quisque cursus, magna vel pharetra dignissim, nunc
-          sapien imperdiet arcu, vitae porta magna nulla quis dolor. Aenean
-          faucibus feugiat ornare. Nulla facilisi. Duis eget velit condimentum,
-          ullamcorper felis ut, varius urna. Sed lacinia tortor arcu, eget
-          ultrices est laoreet vel. Aliquam eget dui non mauris bibendum rutrum.
-          Suspendisse non sodales lorem. Sed consequat commodo nulla sed
-          maximus. Ut convallis tempor lorem, nec lacinia nulla rhoncus vel.
-          Orci varius natoque penatibus et magnis dis parturient montes,
-          nascetur ridiculus mus. Sed ac pharetra ligula. Aliquam consectetur
-          dolor nunc, pharetra facilisis augue malesuada quis. Vivamus cursus
-          lorem nec imperdiet suscipit. Suspendisse volutpat ultrices metus non
-          tristique. Sed pharetra mi non felis tempus, eget aliquam sem
-          scelerisque. Pellentesque suscipit, sapien sit amet semper semper,
-          massa est auctor nisl, a euismod lectus purus et urna. Vivamus et
-          rutrum risus. Donec interdum, purus quis ullamcorper maximus, eros
-          lacus placerat ex, quis dictum dolor magna eu urna. Integer aliquam,
-          quam in facilisis tempus, leo massa cursus orci, ac lobortis mi elit
-          id felis. Nullam molestie malesuada efficitur. Vivamus mi lectus,
-          posuere in feugiat eu, consequat ut nibh. Nam consectetur sapien eu
-          convallis hendrerit. Etiam placerat, leo vitae fringilla mollis, diam
-          mauris vulputate massa, sed venenatis ante libero sit amet lorem.
-          Aliquam ipsum libero, iaculis eu dolor eget, rutrum pulvinar massa.
-          Vestibulum sed lorem orci. Aenean eget tempus ex. Sed tincidunt purus
-          quis ullamcorper ornare. Etiam ac vestibulum odio. Phasellus eu felis
-          quis nisi mollis hendrerit. Nulla consequat odio ac ornare
-          scelerisque. Vestibulum et lorem velit. Ut sit amet erat tincidunt,
-          viverra metus nec, fringilla ipsum. Aliquam sed lorem tristique,
-          eleifend risus fermentum, molestie quam.
-        </p>
+      <div className="p-4 trabajemos">
+        <h1 className="p-4">¡TRABAJEMOS JUNTOS!</h1>
+        <div>
+          <h4 className="p-4">
+            Mi nombre es Jonathan Díaz y quiero formar parte de tu proyecto
+          </h4>
+          <p className="p-4">
+            Me gusta trabajar en equipo, involucrarme en los proyectos y sobre todo... ¡¡El buen rollo!!
+          </p>
+        </div>
       </div>
     </section>
   );
