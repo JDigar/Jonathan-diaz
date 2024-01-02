fed5a2861ca0503f3d7e6d85d4ee72b49dcbc603
JDigar <jdigar96@gmail.com>
2024-01-02 22:07:20 +0000

    video

--- src/front/js/pages/inicio.js
+++ src/front/js/pages/inicio.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../../styles/home.css";
+import ReactPlayer from "react-player";
 
 export const Inicio = () => {
   return (
@@ -9,6 +10,9 @@ export const Inicio = () => {
           <h1 className="h1-inicio"> LIVE GIG MUSIC </h1>
 
           <h2>GRUPOS Y ARTISTAS PARA BODAS Y EVENTOS</h2>
+          <div>
+            <ReactPlayer url='https://www.youtube.com/watch?v=ETuxPF8HKyQ'/>
+          </div>
         </div>
       </div>
 
@@ -16,11 +20,11 @@ export const Inicio = () => {
         <h1 className="p-4">¡TRABAJEMOS JUNTOS!</h1>
         <div>
           <h4 className="p-4">
-            Mi nombre es Jonathan Díaz y quiero formar parte de tu proyecto
+            Estamos deseando poner la banda sonora de vuestro día
           </h4>
           <p className="p-4">
-            Me gusta trabajar en equipo, involucrarme en los proyectos y sobre
-            todo... ¡¡El buen rollo!!
+            Nos encanta hacer disfrutar a la gente y sobre todo... ¡¡El buen
+            rollo!!
           </p>
         </div>
       </div>
