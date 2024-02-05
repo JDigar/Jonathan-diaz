f01088d67cb7d58741c0401598885b404bfb6c4c
JDigar <jdigar96@gmail.com>
2024-02-05 21:53:26 +0000

    cambiando pag inicio

--- src/front/js/component/banner.js
+++ src/front/js/component/banner.js
@@ -10,160 +10,166 @@ import wade from "../../img/wade.jpg";
 export const BanerInicio = () => (
   <div className="col-12 row center banner">
     <div className="cnt-banner row">
-    <div className="col-6 p-5">
-      <div className="col-auto text-center pb-3">
-        <a className="i-banner" href="/proyectos">
-          GRUPOS
-        </a>
-      </div>
-      <div id="carouselExampleIndicators1" className="carousel slide">
-        <div className="carousel-indicators">
+      <div className="col-6 p-5">
+        <div className="col-auto text-center pb-3">
+          <a className="i-banner" href="/proyectos">
+            GRUPOS
+          </a>
+        </div>
+        <div id="carouselExampleIndicators1" className="carousel slide">
+          <div className="carousel-indicators">
+            <button
+              type="button"
+              data-bs-target="#carouselExampleIndicators1"
+              data-bs-slide-to="0"
+              className="active"
+              aria-current="true"
+              aria-label="Slide 1"
+            ></button>
+            <button
+              type="button"
+              data-bs-target="#carouselExampleIndicators1"
+              data-bs-slide-to="1"
+              aria-label="Slide 2"
+            ></button>
+            <button
+              type="button"
+              data-bs-target="#carouselExampleIndicators1"
+              data-bs-slide-to="2"
+              aria-label="Slide 3"
+            ></button>
+          </div>
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img
+                src={vetusta}
+                className="d-block w-100"
+                alt="VETUSTA MORLA"
+              />
+              <div className="carousel-caption d-none d-md-block">
+                <h5>Vetusta Morla</h5>
+                <p>Banda Madrileña con canciones propias</p>
+              </div>
+            </div>
+            <div className="carousel-item">
+              <img src={arde} className="d-block w-100" alt="..." />
+              <div className="carousel-caption d-none d-md-block">
+                <h5>Arde Bogotá</h5>
+                <p>Banda de Cartagena, composiciones propias</p>
+              </div>
+            </div>
+            <div className="carousel-item">
+              <img src={moda} className="d-block w-100" alt="..." />
+              <div className="carousel-caption d-none d-md-block">
+                <h5>La M.O.D.A</h5>
+                <p>La Maravilla Orquesta Del Alcohol</p>
+              </div>
+            </div>
+          </div>
           <button
+            className="carousel-control-prev"
             type="button"
             data-bs-target="#carouselExampleIndicators1"
-            data-bs-slide-to="0"
-            className="active"
-            aria-current="true"
-            aria-label="Slide 1"
-          ></button>
+            data-bs-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
           <button
+            className="carousel-control-next"
             type="button"
             data-bs-target="#carouselExampleIndicators1"
-            data-bs-slide-to="1"
-            aria-label="Slide 2"
-          ></button>
-          <button
-            type="button"
-            data-bs-target="#carouselExampleIndicators1"
-            data-bs-slide-to="2"
-            aria-label="Slide 3"
-          ></button>
+            data-bs-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Next</span>
+          </button>
         </div>
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <img src={vetusta} className="d-block w-100" alt="VETUSTA MORLA" />
-            <div className="carousel-caption d-none d-md-block">
-              <h5>Vetusta Morla</h5>
-              <p>Banda Madrileña con canciones propias</p>
-            </div>
-          </div>
-          <div className="carousel-item">
-            <img src={arde} className="d-block w-100" alt="..." />
-            <div className="carousel-caption d-none d-md-block">
-              <h5>Arde Bogotá</h5>
-              <p>Banda de Cartagena, composiciones propias</p>
-            </div>
-          </div>
-          <div className="carousel-item">
-            <img src={moda} className="d-block w-100" alt="..." />
-            <div className="carousel-caption d-none d-md-block">
-              <h5>La M.O.D.A</h5>
-              <p>La Maravilla Orquesta Del Alcohol</p>
-            </div>
-          </div>
+      </div>
+      <div className="col-6 p-5">
+        <div className="col-auto text-center pb-3">
+          <a className="i-banner" href="/about-me">
+            DJ'S
+          </a>
         </div>
-        <button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExampleIndicators1"
-          data-bs-slide="prev"
-        >
-          <span
-            className="carousel-control-prev-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExampleIndicators1"
-          data-bs-slide="next"
-        >
-          <span
-            className="carousel-control-next-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Next</span>
-        </button>
-      </div>
-    </div>
-    <div className="col-6 p-5">
-      <div className="col-auto text-center pb-3">
-        <a className="i-banner">DJ'S</a>
-      </div>
-      <div id="carouselExampleIndicators2" className="carousel slide">
-        <div className="carousel-indicators">
+        <div id="carouselExampleIndicators2" className="carousel slide">
+          <div className="carousel-indicators">
+            <button
+              type="button"
+              data-bs-target="#carouselExampleIndicators2"
+              data-bs-slide-to="0"
+              className="active"
+              aria-current="true"
+              aria-label="Slide 1"
+            ></button>
+            <button
+              type="button"
+              data-bs-target="#carouselExampleIndicators2"
+              data-bs-slide-to="1"
+              aria-label="Slide 2"
+            ></button>
+            <button
+              type="button"
+              data-bs-target="#carouselExampleIndicators2"
+              data-bs-slide-to="2"
+              aria-label="Slide 3"
+            ></button>
+          </div>
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img src={wade} className="d-block w-100" alt="..." />
+              <div className="carousel-caption d-none d-md-block">
+                <h5>Wade</h5>
+                <p>DJ y Productor Sevillano, residente de El Row</p>
+              </div>
+            </div>
+            <div className="carousel-item">
+              <img src={kid} className="d-block w-100" alt="..." />
+              <div className="carousel-caption d-none d-md-block">
+                <h5>Kid Rizzo</h5>
+                <p>DJ Valenciano para Bodas y Eventos</p>
+              </div>
+            </div>
+            <div className="carousel-item">
+              <img src={timmy} className="d-block w-100" alt="..." />
+              <div className="carousel-caption d-none d-md-block">
+                <h5>Timmy Trumpet</h5>
+                <p>Icónico DJ Australiano</p>
+              </div>
+            </div>
+          </div>
           <button
+            className="carousel-control-prev"
             type="button"
             data-bs-target="#carouselExampleIndicators2"
-            data-bs-slide-to="0"
-            className="active"
-            aria-current="true"
-            aria-label="Slide 1"
-          ></button>
+            data-bs-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
           <button
+            className="carousel-control-next"
             type="button"
             data-bs-target="#carouselExampleIndicators2"
-            data-bs-slide-to="1"
-            aria-label="Slide 2"
-          ></button>
-          <button
-            type="button"
-            data-bs-target="#carouselExampleIndicators2"
-            data-bs-slide-to="2"
-            aria-label="Slide 3"
-          ></button>
+            data-bs-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Next</span>
+          </button>
         </div>
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <img src={wade} className="d-block w-100" alt="..." />
-            <div className="carousel-caption d-none d-md-block">
-              <h5>Wade</h5>
-              <p>DJ y Productor Sevillano, residente de El Row</p>
-            </div>
-          </div>
-          <div className="carousel-item">
-            <img src={kid} className="d-block w-100" alt="..." />
-            <div className="carousel-caption d-none d-md-block">
-              <h5>Kid Rizzo</h5>
-              <p>DJ Valenciano para Bodas y Eventos</p>
-            </div>
-          </div>
-          <div className="carousel-item">
-            <img src={timmy} className="d-block w-100" alt="..." />
-            <div className="carousel-caption d-none d-md-block">
-              <h5>Timmy Trumpet</h5>
-              <p>Icónico DJ Australiano</p>
-            </div>
-          </div>
-        </div>
-        <button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExampleIndicators2"
-          data-bs-slide="prev"
-        >
-          <span
-            className="carousel-control-prev-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExampleIndicators2"
-          data-bs-slide="next"
-        >
-          <span
-            className="carousel-control-next-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Next</span>
-        </button>
       </div>
-    </div>
     </div>
   </div>
 );
--- src/front/js/pages/inicio.js
+++ src/front/js/pages/inicio.js
@@ -9,28 +9,33 @@ export const Inicio = () => {
     <section id="home">
       <div className="">
         <div>
-            <Welcome />
-        </div>
+          <Welcome />
+        </div>{" "}
         <div>
-            <Space />
-        </div>
+          <Space />
+        </div>{" "}
         <div>
-            <BanerInicio />
-        </div>
+          <BanerInicio />
+        </div>{" "}
       </div>
-
       <div className="p-4 trabajemos">
-        <a className="p-4 a-trabajemos">¡TRABAJEMOS JUNTOS!</a>
+        <a className="p-4 a-trabajemos" href="/contact">
+          {" "}
+          ¡TRABAJEMOS JUNTOS!{" "}
+        </a>{" "}
         <div>
           <h4 className="p-4">
-            Estamos deseando poner la banda sonora de vuestro día
-          </h4>
+            Estamos deseando poner la banda sonora de vuestro día{" "}
+          </h4>{" "}
           <p className="p-4">
-            Nos encanta hacer disfrutar a la gente y sobre todo... ¡¡El buen
+            Nos encanta hacer disfrutar a la gente y sobre todo...¡¡El buen
             rollo!!
-          </p>
-        </div>
-      </div>
+          </p>{" "}
+          <a className=" anavbar contacto-inicio" href="/contact">
+            CUÉNTAME QUE QUIERES PARA TU DÍA{" "}
+          </a>{" "}
+        </div>{" "}
+      </div>{" "}
     </section>
   );
 };
